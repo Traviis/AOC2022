3fda83948736289952007c568dc631bb7ea680f7
Travis Taylor <travis@trickfire.com>
2022-12-01 12:32:52 -0700

    Complete day1

---
+++ src/day1.rs
@@ -0,0 +1,67 @@
+use std::fs;
+
+
+// Let's over-engineer this for explicitness.
+type Calories = u64;
+type ElfBackpack = Vec<Calories>;
+
+#[aoc_generator(day1)]
+fn day1_parse(input: &str) -> Vec<ElfBackpack> {
+    input
+        .split("\n\n")
+        .map(|bp| {
+            bp.split("\n")
+                .map(|n| n.parse::<Calories>().unwrap())
+                .collect::<ElfBackpack>()
+        })
+        .collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &Vec<ElfBackpack>) -> u64 {
+    input
+        .iter()
+        .map(|bp| bp.iter().sum::<Calories>())
+        .max()
+        .unwrap()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &Vec<ElfBackpack>) -> u64 {
+    let mut sums: Vec<u64> = input.iter().map(|bp| bp.iter().sum::<Calories>()).collect();
+    sums.sort();
+    sums.iter().rev().take(3).sum()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn get_test_input() -> &'static str {
+        "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"
+    }
+
+    #[test]
+    fn day1_part1() {
+        assert_eq!(part1(&day1_parse(get_test_input())), 24000);
+    }
+
+    #[test]
+    fn day1_part2() {
+        assert_eq!(part2(&day1_parse(get_test_input())), 45000);
+    }
+}
--- src/template.rs
+++ src/template.rs
@@ -1,15 +1,19 @@
+
+type InputType = Vec<&str>;
+type OutputType = u64;
+
 #[aoc_generator(day1)]
-fn day1_parse(input: &str) -> Vec<&str> {
+fn day1_parse(input: &str) -> InputType {
     todo!();
 }
 
 #[aoc(day1, part1)]
-pub fn part1(input: &Vec<&str>) -> usize {
+pub fn part1(input: &InputType) -> OutputType {
     todo!();
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &Vec<&str>) -> usize {
+pub fn part2(input: &InputType) -> OutputType {
     todo!();
 }
 
@@ -24,11 +28,11 @@ mod tests {
 
     #[test]
     fn example_part1() {
-        assert_eq!(part1(get_test_input()), 7);
+        assert_eq!(part1(day1_parse(get_test_input())), 0);
     }
 
     #[test]
     fn example_part2() {
-        assert_eq!(part2(get_test_input()), 5);
+        assert_eq!(part2(day1_parse(get_test_input())), 0);
     }
 }

d0782041d4e2eb9c8d8de72fa1238b35711c7440
Travis Taylor <travis@trickfire.com>
2022-12-01 11:43:27 -0700

    Add start of 2022

---
+++ src/template.rs
@@ -0,0 +1,34 @@
+#[aoc_generator(day1)]
+fn day1_parse(input: &str) -> Vec<&str> {
+    todo!();
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &Vec<&str>) -> usize {
+    todo!();
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &Vec<&str>) -> usize {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn get_test_input() -> &'static str {
+        todo!();
+    }
+
+    #[test]
+    fn example_part1() {
+        assert_eq!(part1(get_test_input()), 7);
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(part2(get_test_input()), 5);
+    }
+}

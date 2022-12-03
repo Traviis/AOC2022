fb44ca0acc9d940175b362e6fafa0e89b5c534d8
Travis Taylor <travis@trickfire.com>
2022-12-03 01:20:45 -0700

    Impl variant on my solution for a minor but signfigant speedup.

--- src/day3.rs
+++ src/day3.rs
@@ -1,16 +1,74 @@
-
 use std::collections::{HashMap, HashSet};
 
-pub type InputType = Vec<Rucksack>;
+pub type InputType = Vec<RucksackSet>;
 pub type OutputType = usize;
 
 pub struct Rucksack {
-    //I thought I would need to count the duplicates, I guess not.
-    first: HashMap<char,usize>,
-    second: HashMap<char,usize>,
+    //I thought I would need to count the duplicates, I guess not. I could have Just done sets the
+    //entire time
+    first: HashMap<char, usize>,
+    second: HashMap<char, usize>,
 }
 
-impl Rucksack {
+//Implement the Set only version that doesn't care about counts to see performance enhancements
+pub struct RucksackSet {
+    first: HashSet<char>,
+    second: HashSet<char>,
+}
+
+// Rucksack bench
+// Day3 - Part1/(default)  time:   [112.59 us 112.70 us 112.81 us]
+// Day3 - Part2/(default)  time:   [258.37 us 258.73 us 259.19 us]
+// Generator               time:   [255.17 us 255.97 us 256.98 us]
+//
+// RucksackSet
+// Day3 - Part1/(default)  time:   [25.117 us 25.159 us 25.201 us]
+// Day3 - Part2/(default)  time:   [176.10 us 176.28 us 176.47 us]
+// Generator               time:   [194.21 us 194.63 us 195.05 us]
+
+pub trait Sack
+where
+    Self: Sized,
+{
+    fn new(ln: &str) -> Self;
+    fn find_duplicates(&self) -> Vec<char>;
+    fn get_combined_set(&self) -> HashSet<char>;
+    fn find_item_in_three(&self, second: &Self, third: &Self) -> Option<char>;
+}
+
+impl Sack for RucksackSet {
+    fn new(ln: &str) -> Self {
+        let len = ln.chars().count();
+        assert_eq!(len % 2, 0);
+        let mut cs = ln.chars();
+        let first = cs.by_ref().take(len / 2).collect::<HashSet<char>>();
+        let second = cs.by_ref().take(len / 2).collect::<HashSet<char>>();
+        RucksackSet { first, second }
+    }
+    fn find_duplicates(&self) -> Vec<char> {
+        self.first.intersection(&self.second).map(|c| *c).collect()
+    }
+
+    fn get_combined_set(&self) -> HashSet<char> {
+        self.first.union(&self.second).map(|c| *c).collect()
+    }
+
+    fn find_item_in_three(&self, second: &Self, third: &Self) -> Option<char> {
+        let f = self
+            .get_combined_set()
+            .intersection(&second.get_combined_set())
+            .map(|c| *c)
+            .collect::<HashSet<char>>();
+        let final_set = f
+            .intersection(&third.get_combined_set())
+            .map(|c| *c)
+            .collect::<Vec<_>>();
+        assert!(final_set.iter().count() <= 1);
+        final_set.iter().next().copied()
+    }
+}
+
+impl Sack for Rucksack {
     fn new(ln: &str) -> Self {
         let len = ln.chars().count();
         assert_eq!(len % 2, 0);
@@ -19,7 +77,9 @@ impl Rucksack {
         let mut first = HashMap::new();
         let mut second = HashMap::new();
 
-        cs.by_ref().take(len / 2).for_each(|c| *first.entry(c).or_insert(0) += 1);
+        cs.by_ref()
+            .take(len / 2)
+            .for_each(|c| *first.entry(c).or_insert(0) += 1);
         cs.for_each(|c| *second.entry(c).or_insert(0) += 1);
 
         //println!("{:?} -- {:?}", first, second);
@@ -28,29 +88,50 @@ impl Rucksack {
     }
 
     fn find_duplicates(&self) -> Vec<char> {
-        let first_set = self.first.iter().map(|(k,_)| *k).collect::<HashSet<char>>();
-        let second_set = self.second.iter().map(|(k,_)| *k).collect::<HashSet<char>>();
+        let first_set = self
+            .first
+            .iter()
+            .map(|(k, _)| *k)
+            .collect::<HashSet<char>>();
+        let second_set = self
+            .second
+            .iter()
+            .map(|(k, _)| *k)
+            .collect::<HashSet<char>>();
         first_set.intersection(&second_set).map(|c| *c).collect()
     }
 
     fn get_combined_set(&self) -> HashSet<char> {
-        let first_set = self.first.iter().map(|(k,_)| *k).collect::<HashSet<char>>();
-        let second_set = self.second.iter().map(|(k,_)| *k).collect::<HashSet<char>>();
+        let first_set = self
+            .first
+            .iter()
+            .map(|(k, _)| *k)
+            .collect::<HashSet<char>>();
+        let second_set = self
+            .second
+            .iter()
+            .map(|(k, _)| *k)
+            .collect::<HashSet<char>>();
         first_set.union(&second_set).map(|c| *c).collect()
     }
 
     fn find_item_in_three(&self, second: &Self, third: &Self) -> Option<char> {
-        let first_two = self.get_combined_set().intersection(&second.get_combined_set()).map(|c| *c).collect::<HashSet<char>>();
-        let final_set = first_two.intersection(&third.get_combined_set()).map(|c| *c).collect::<Vec<_>>();
+        let first_two = self
+            .get_combined_set()
+            .intersection(&second.get_combined_set())
+            .map(|c| *c)
+            .collect::<HashSet<char>>();
+        let final_set = first_two
+            .intersection(&third.get_combined_set())
+            .map(|c| *c)
+            .collect::<Vec<_>>();
         assert!(final_set.iter().count() <= 1);
 
         final_set.iter().next().copied()
-
     }
 }
 
 fn convert_char_to_priority(c: &char) -> usize {
-
     //println!("{}",c);
     //I know, it's horrible
     if *c == '_' {
@@ -65,33 +146,44 @@ fn convert_char_to_priority(c: &char) -> usize {
     } else {
         val - HIGHER_OFFSET
     }
-
-
 }
 
 #[aoc_generator(day3)]
 fn day3_parse(input: &str) -> InputType {
-    input.split("\n").map(|line| Rucksack::new(line)).collect()
+    input
+        .split("\n")
+        .map(|line| RucksackSet::new(line))
+        .collect()
 }
 
 #[aoc(day3, part1)]
 pub fn part1(input: &InputType) -> OutputType {
+    part1_tmpl::<RucksackSet>(input)
+}
+
+pub fn part1_tmpl<T: Sack>(input: &InputType) -> OutputType {
     input
         .iter()
-        .map(|r| r.find_duplicates()
-            .iter()
-            .map(|c| convert_char_to_priority(c)).sum::<usize>()
-        )
+        .map(|r| {
+            r.find_duplicates()
+                .iter()
+                .map(|c| convert_char_to_priority(c))
+                .sum::<usize>()
+        })
         .sum::<usize>()
 }
 
 #[aoc(day3, part2)]
 pub fn part2(input: &InputType) -> OutputType {
+    part2_tmpl::<RucksackSet>(input)
+}
+
+pub fn part2_tmpl<T: Sack>(input: &Vec<T>) -> OutputType {
     input
         .chunks(3)
         .map(|three_rucks| match &three_rucks {
-            &[a,b,c] => convert_char_to_priority(&a.find_item_in_three(&b,&c).unwrap_or('_')),
-            _ => panic!("Can this even happen?")
+            &[a, b, c] => convert_char_to_priority(&a.find_item_in_three(&b, &c).unwrap_or('_')),
+            _ => panic!("Can this even happen?"),
         })
         .sum()
 }

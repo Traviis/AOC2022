fbd56783f95befa392d34d7f3375ab978837eafb
Travis Taylor <travis@trickfire.com>
2022-12-11 23:30:48 -0700

    I'm too tired to actually implement this right now

--- src/day12.rs
+++ src/day12.rs
@@ -1,15 +1,132 @@
+use std::collections::{HashMap, HashSet, VecDeque};
 
-type InputType = Vec<&str>;
-type OutputType = u64;
+type InputType = Vec<Vec<char>>;
+type OutputType = usize;
+
+type Coordinate = (usize, usize);
 
 #[aoc_generator(day12)]
 fn day12_parse(input: &str) -> InputType {
-    todo!();
+    input
+        .split("\n")
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<_>>()
+}
+
+fn find_start(grid: &Vec<Vec<char>>) -> (usize, usize) {
+    find_char('S', grid)
+}
+
+fn find_end(grid: &Vec<Vec<char>>) -> (usize, usize) {
+    find_char('E', grid)
+}
+
+fn find_char(ch: char, grid: &Vec<Vec<char>>) -> (usize, usize) {
+    for (y, ve) in grid.iter().enumerate() {
+        for (x, c) in ve.iter().enumerate() {
+            if ch == *c {
+                return (x, y);
+            }
+        }
+    }
+    panic!("S not found")
+}
+
+fn find_candidates(
+    input: &InputType,
+    (x, y): Coordinate,
+    max_x: usize,
+    max_y: usize,
+) -> Vec<Coordinate> {
+    //Don't care about diagonals, just left, right, up, down
+    let mut candidates = vec![];
+    let cur_height = input[y][x] as i32; //cast char to some value
+    println!("Max x: {} max y: {}", max_x, max_y);
+    for (c_x, c_y) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+        let cand = (x as i32 + c_x, y as i32 + c_y);
+        println!(
+            "Examine candidate node ({},{}) cur: {}",
+            cand.0, cand.1, cur_height
+        );
+        if cand.0 >= max_x as i32 || cand.1 >= max_y as i32 || cand.0 < 0 || cand.1 < 0 {
+            continue;
+        }
+        // colummn major
+        let val = *(input
+            .get(cand.1 as usize)
+            .unwrap()
+            .get(cand.0 as usize)
+            .unwrap()) as i32;
+        println!(
+            "cur: {}, val: {}, al: {}",
+            cur_height,
+            val,
+            cur_height - val as i32
+        );
+        if (cur_height - val as i32).abs() <= 1 {
+            candidates.push(cand);
+        }
+    }
+
+    candidates
+        .iter()
+        .map(|(x, y)| (*x as usize, *y as usize))
+        .collect()
 }
 
 #[aoc(day12, part1)]
 pub fn part1(input: &InputType) -> OutputType {
-    todo!();
+    let mut input = input.clone();
+
+    let (s_x, s_y) = find_start(&input);
+    let (e_x, e_y) = find_end(&input);
+
+    println!("Start ({},{}) End ({},{})", s_x, s_y, e_x, e_y);
+
+    input[s_y][s_x] = 'a';
+    input[e_y][e_x] = 'z'; //set proper heights
+
+    let max_x = input.get(0).unwrap().iter().count() - 1;
+    let max_y = input.iter().count() - 1;
+
+    let mut visited: HashSet<Coordinate> = HashSet::new();
+    let mut to_examine: VecDeque<Coordinate> = VecDeque::new();
+    let mut distance: HashMap<Coordinate, usize> = HashMap::new();
+
+    distance.insert((s_x, s_y), 0);
+
+    static ARBITRARY_HIGH_COST: usize = 5000;
+
+    to_examine.push_back((s_x, s_y));
+
+    while let Some(c_node) = to_examine.pop_front() {
+        //Technically I need the minimum distance for the available vertices
+        visited.insert((c_node.0, c_node.1));
+        let candidate_nodes = find_candidates(&input, c_node, max_x, max_y);
+        println!("Candidates for {:?}: {:?}", c_node, candidate_nodes);
+        //Assume that all candidates are 1, since that's the only distance this function should
+        //return (that or 0)
+        for cand in candidate_nodes.iter() {
+            if visited.get(cand).is_some() {
+                println!("Skip existing {:?}", cand);
+                continue; //skip existing
+            }
+            let cand_dist = distance
+                .get(&(cand.0, cand.1))
+                .unwrap_or(&ARBITRARY_HIGH_COST)
+                + 1;
+            println!("Distance to ({},{}) = {}", cand.0, cand.1, cand_dist);
+            if cand_dist < *distance.get(&c_node).unwrap_or(&ARBITRARY_HIGH_COST) {
+                distance.insert(c_node, cand_dist);
+            }
+
+            to_examine.push_back((cand.0, cand.1));
+        }
+        //Don't allow already visisted
+    }
+
+    println!("dist: {:?}", distance);
+    *(distance.get(&(e_x, e_y)).unwrap())
 }
 
 #[aoc(day12, part2)]
@@ -23,12 +140,16 @@ mod tests {
     use super::*;
 
     fn get_test_input() -> &'static str {
-        todo!();
+        "Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"
     }
 
     #[test]
     fn day12_part1() {
-        assert_eq!(part1(&day12_parse(get_test_input())), 0);
+        assert_eq!(part1(&day12_parse(get_test_input())), 31);
     }
 
     #[test]

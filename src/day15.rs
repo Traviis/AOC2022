213426d0d616b9d89a8c39c184543ba4ec2a5395
Travis Taylor <travis@trickfire.com>
2022-12-16 13:58:04 -0700

    Eh, I gave up on this one and used someone elses solution. I get the gist, but I would have never been able to really come upw ith the math for that on my own.

--- src/day15.rs
+++ src/day15.rs
@@ -8,7 +8,8 @@ use self::rayon::prelude::*;
 use std::sync::Arc;
 use std::sync::Mutex;
 
-use std::cmp;
+use std::convert::TryFrom;
+
 
 #[derive(Debug)]
 pub enum Item {
@@ -17,11 +18,8 @@ pub enum Item {
     Nothing,
 }
 
-type InputType = (
-    HashMap<(i128, i128), Item>,
-    HashMap<(i128, i128), (i128, i128)>,
-);
-type OutputType = i128;
+type InputType = (HashMap<(i32, i32), Item>, HashMap<(i32, i32), (i32, i32)>);
+type OutputType = i32;
 
 #[aoc_generator(day15)]
 fn day15_parse(input: &str) -> InputType {
@@ -40,7 +38,7 @@ fn day15_parse(input: &str) -> InputType {
         let mut caps = caps
             .iter()
             .skip(1)
-            .map(|c| c.unwrap().as_str().parse::<i128>().unwrap());
+            .map(|c| c.unwrap().as_str().parse::<i32>().unwrap());
 
         let s_x = caps.next().unwrap();
         let s_y = caps.next().unwrap();
@@ -56,20 +54,20 @@ fn day15_parse(input: &str) -> InputType {
 }
 
 fn print_debug_map(
-    map: &HashMap<(i128, i128), Item>,
+    map: &HashMap<(i32, i32), Item>,
     max_x: usize,
     max_y: usize,
-    sens_range: &HashSet<(i128, i128)>,
+    sens_range: &HashSet<(i32, i32)>,
 ) {
-    for y in -2..=max_y as i128 {
-        for x in -2..=max_x as i128 {
+    for y in -2..=max_y as i32 {
+        for x in -2..=max_x as i32 {
             print!(
                 "{}",
-                match map.get(&(x as i128, y as i128)).unwrap_or(&Item::Nothing) {
+                match map.get(&(x as i32, y as i32)).unwrap_or(&Item::Nothing) {
                     Item::Sensor => "S",
                     Item::Beacon => "B",
                     Item::Nothing =>
-                        if sens_range.get(&(x as i128, y as i128)).is_some() {
+                        if sens_range.get(&(x as i32, y as i32)).is_some() {
                             "#"
                         } else {
                             "."
@@ -81,54 +79,119 @@ fn print_debug_map(
     }
 }
 
-fn manhatten((x1, y1): (i128, i128), (x2, y2): (i128, i128)) -> i128 {
+fn manhatten((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> i32 {
     (x1 - x2).abs() + (y1 - y2).abs()
 }
 
 fn bounded_naive_determine_sensor_range(
-    (s_x, s_y): (i128, i128),
-    (b_x, b_y): (i128, i128),
-    min_x: i128,
-    max_x: i128,
-    min_y: i128,
-    max_y: i128,
-) -> HashSet<(i128, i128)> {
+    (s_x, s_y): (i32, i32),
+    (b_x, b_y): (i32, i32),
+    min_x: i32,
+    max_x: i32,
+    min_y: i32,
+    max_y: i32,
+) -> HashSet<(i32, i32)> {
     let dist = manhatten((s_x, s_y), (b_x, b_y));
 
     //We only care about a very specific y coordinate
 
     //TODO: This can be optimized more, but let's just try to brute force it with checking the row
     let mut out = HashSet::new();
-    out.insert((s_x,s_y));
+    out.insert((s_x, s_y));
     //for m_y in s_y - dist..=s_y + dist {
     for m_y in min_y..=max_y {
         for m_x in s_x - dist..=s_x + dist {
             if manhatten((m_x, m_y), (s_x, s_y)) <= dist {
-                    out.insert((m_x, m_y));
+                out.insert((m_x, m_y));
             }
         }
     }
     out
 }
+
+fn find_sensor_range_intersections(
+    closest_map: &HashMap<(i32, i32), (i32, i32)>,
+    max_coord: i32,
+) -> i32 {
+    let sensors = &closest_map
+        .iter()
+        .map(|((sx, sy), (bx, by))| ((i32::try_from(*sx).unwrap(), i32::try_from(*sy).unwrap()), (i32::try_from(*bx).unwrap(), i32::try_from(*by).unwrap())))
+        .collect::<Vec<((i32, i32), (i32, i32))>>()[..];
+
+    //This is failing..? Is my input bad?
+
+    //Wish I could take credit for this, but I was just tired of this problem and found someone
+    //elses solution: https://github.com/emlun/adventofcode-2022/blob/9dadc35bb4ebbb8352f9525f0eeaf2ea2babd766/src/days/day15.rs#L57-L91
+    //Reddit thread with explanation: https://www.reddit.com/r/adventofcode/comments/zmw9d8/2022_day_15_part_2_speed_up_your_solution_60000x/
+    for (i1, ((sx1, sy1), (bx1, by1))) in sensors.iter().enumerate() {
+        let r1: i32 = i32::try_from(sx1.abs_diff(*bx1) + sy1.abs_diff(*by1)).unwrap();
+
+        for ((sx2, sy2), (bx2, by2)) in sensors[i1 + 1..].iter() {
+            let r2: i32 = i32::try_from(sx2.abs_diff(*bx2) + sy2.abs_diff(*by2)).unwrap();
+            let b1s: [i32; 4] = [
+                r2 + sx2 + sy2 + 1,
+                -r2 + sx2 + sy2 - 1,
+                r2 + sx2 + sy2 + 1,
+                -r2 + sx2 + sy2 - 1,
+            ];
+            let b2s: [i32; 4] = [
+                -r1 + sx1 - sy1 - 1,
+                -r1 + sx1 - sy1 - 1,
+                r1 + sx1 - sy1 + 1,
+                r1 + sx1 - sy1 + 1,
+            ];
+
+            for (b1, b2) in b1s.iter().zip(b2s) {
+                let x = (b1 + b2) / 2;
+                let y = (b1 - b2) / 2;
+
+                if (0..=max_coord).contains(&x)
+                    && (0..=max_coord).contains(&y)
+                    && sensors.iter().all(|((sx, sy), (bx, by))| {
+                        let r = sx.abs_diff(*bx) + sy.abs_diff(*by);
+                        let d = sx.abs_diff(x) + sy.abs_diff(y);
+                        d > r
+                    })
+                {
+                    return i32::from(x) * 4000000 + i32::from(y);
+                }
+            }
+        }
+    }
+
+    // If solution is not on the boundary of two sensors,
+    // it must be in a corner of the permitted region.
+    for x in [0, max_coord] {
+        for y in [0, max_coord] {
+            if sensors.iter().all(|((sx, sy), (bx, by))| {
+                let r = sx.abs_diff(*bx) + sy.abs_diff(*by);
+                let d = sx.abs_diff(x) + sy.abs_diff(y);
+                d > r
+            }) {
+                return i32::from(x) * 4000000 + i32::from(y);
+            }
+        }
+    }
+
+    unimplemented!();
+}
+
 fn naive_determine_sensor_range(
-    (s_x, s_y): (i128, i128),
-    (b_x, b_y): (i128, i128),
-    cared_y: i128,
-) -> HashSet<(i128, i128)> {
+    (s_x, s_y): (i32, i32),
+    (b_x, b_y): (i32, i32),
+    cared_y: i32,
+) -> HashSet<(i32, i32)> {
     //There is probably a clever way of calculating this, but brute force this bad boy!
     bounded_naive_determine_sensor_range(
         (s_x, s_y),
         (b_x, b_y),
-        i128::MIN,
-        i128::MAX,
+        i32::MIN,
+        i32::MAX,
         cared_y,
         cared_y,
     )
 }
-fn determine_sensor_range(
-    (s_x, s_y): (i128, i128),
-    (b_x, b_y): (i128, i128),
-) -> HashSet<(i128, i128)> {
+fn determine_sensor_range((s_x, s_y): (i32, i32), (b_x, b_y): (i32, i32)) -> HashSet<(i32, i32)> {
     let dist = manhatten((s_x, s_y), (b_x, b_y));
     todo!();
 }
@@ -138,11 +201,8 @@ pub fn part1(inp: &InputType) -> OutputType {
     part1_param(inp, 2000000)
 }
 
-//TODO: Ok, the slam it together approach sucks, I should rewrite this to not dump everything into
-//a set, but instead just write a function that will determine if a given point is within sensor
-//range.
 
-pub fn part1_param((map, closest_map): &InputType, cared_y: i128) -> OutputType {
+pub fn part1_param((map, closest_map): &InputType, cared_y: i32) -> OutputType {
     let mut all_ranges = Arc::new(Mutex::new(HashSet::new()));
 
     closest_map
@@ -156,67 +216,24 @@ pub fn part1_param((map, closest_map): &InputType, cared_y: i128) -> OutputType 
             *new_set = (*new_set)
                 .union(&h_set)
                 .map(|x| *x)
-                .collect::<HashSet<(i128, i128)>>();
+                .collect::<HashSet<(i32, i32)>>();
         });
-
 
     let cant_be = all_ranges.lock().unwrap();
 
-   cant_be 
+    cant_be
         .iter()
         .filter(|(_, y)| *y == cared_y)
         .filter(|(x, y)| map.get(&(*x, *y)).is_none())
-        .count() as i128
+        .count() as i32
 }
 
 #[aoc(day15, part2)]
 pub fn part2(inp: &InputType) -> OutputType {
     part2_param(inp, 2000000)
 }
-pub fn part2_param((map, closest_map): &InputType, max_v: i128) -> OutputType {
-    let mut g_x = 0;
-    let mut g_y = 0;
-
-    for c_y in 0..max_v {
-        let mut all_ranges = Arc::new(Mutex::new(HashSet::new()));
-        closest_map
-            .par_iter()
-            .map(|((s_x, s_y), (b_x, b_y))| {
-                bounded_naive_determine_sensor_range((*s_x, *s_y), (*b_x, *b_y), 0, max_v, c_y, c_y)
-                //naive_determine_sensor_range((*s_x, *s_y), (*b_x, *b_y),  c_y)
-            })
-            .for_each(|h_set| {
-                let ranges = Arc::clone(&all_ranges);
-                let mut new_set = ranges.lock().unwrap();
-                *new_set = (*new_set)
-                    .union(&h_set)
-                    .map(|x| *x)
-                    .collect::<HashSet<(i128, i128)>>();
-            });
-
-        let cant_be = all_ranges.lock().unwrap();
-
-        let cant_be_here = cant_be
-            .iter()
-            .filter(|(_, y)| *y == c_y)
-            .filter(|(x, y)| map.get(&(*x, *y)).is_none());
-
-            let set = cant_be_here.map(|x| *x).collect::<HashSet<(i128,i128)>>();
-
-            //println!("{:?}", closest_map.keys());
-
-            for x in 0..=max_v {
-                if set.get(&(x,c_y)).is_none() && closest_map.get(&(x,c_y)).is_none() {
-                    g_x = x;
-                    g_y = c_y;
-                    println!("possible here: {},{} ({})",g_x,g_y,c_y);
-                }
-        }
-
-    }
-    static MULT: i128 = 4000000;
-    println!("Viable {},{}", g_x, g_y);
-    (g_x * MULT) + g_y
+pub fn part2_param((map, closest_map): &InputType, max_v: i32) -> OutputType {
+    find_sensor_range_intersections(closest_map, max_v)
 }
 
 #[cfg(test)]
